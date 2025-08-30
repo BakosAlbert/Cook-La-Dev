4ca7cc61b603cd163fe2f0cd216e6a6456e927b4
Bakos Albert István <bakosalbert2003@gmail.com>
2025-08-30 21:07:26 +0200

    A projekt alapjai újra feltöltve

---
+++ GameLogic/recepies.js
@@ -0,0 +1,28 @@
+//A játékban szereplő receptek listája
+export const recipes = [
+  {
+    name: "Classic Burger",
+    ingredients: ["hamburgerbuns", "chopped_beefmeat", "chopped_tomato", "chopped_lettuce", "cheese"],
+  },
+  {
+    name: "Cheeseburger",
+    ingredients: ["hamburgerbuns", "chopped_beefmeat"],
+  },
+
+  {
+    name: "Basic Sushi",
+    ingredients: ["rice", "chopped_fishmeat", "chopped_lettuce"],
+  },
+  {
+    name: "Veggie Sushi",
+    ingredients: ["rice", "chopped_tomato", "chopped_lettuce"],
+  },
+  {
+    name: "Steak",
+    ingredients: ["beefmeat"],
+  },
+  {
+    name: "Rizotto",
+    ingredients: ["rice", "chopped_chickenmeat"]
+  },
+]
