c67b941a579dc167e5154f005760409273dea9a1
Bakos Albert István <bakosalbert2003@gmail.com>
2025-09-18 02:54:48 +0200

    Mobile irányítás teszt1

---
+++ GameLogic/Pan.js
@@ -0,0 +1,15 @@
+class Pan{
+    constructor(ingredient){
+        this.texture="./assets/kitchenequipments/pan.png";
+        this.ingredient=ingredient;
+    }
+    addingredient(){
+        return;
+    }
+    baking(){
+        return;
+    }
+
+
+
+}
